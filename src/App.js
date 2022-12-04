9f9618712e6397cc429518dad2ba0f020b49775e
imonkfcwifi <nohyungkorea@gmail.com>
2022-12-04 18:01:29 +0900

    toDos, ...[] array

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
-import { useState, useEffect } from "react";
-
-const Hello = () => {
-  const destroyFc = () => {
-    console.log("destroyed : - <");
-  };
-  const effectFc = () => {
-    console.log("created : - >");
-    return destroyFc;
-  };
-  useEffect(effectFc, []);
-  return <h1>Hello</h1>;
-};
+import { useState } from "react";
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setTodo] = useState("");
+  const [toDos, setTodos] = useState([]);
+  const onChange = (event) => setTodo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+
+    setTodos((currenArray) => [toDo, ...currenArray]);
+    setTodo("");
+    // todos.pust <- NEVER
+  };
+  console.log(toDos);
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>오늘 할 일 {toDos.length} 개</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          text="Text"
+          placeholder="Write your to do"
+        />
+        <button>Add To Do</button>
+      </form>
+      {/* form은 submit 이벤트가 있음 */}
     </div>
   );
 }
-// input 을 만들어 event listen을 하고 listen의 function이 작동할때 onChange = (event)<- argument로 event를 받는다
-// event를 발생시킨 input의 target.value를 setKeyword에서 받아서 조작해서 keyword state에 넣어준다.  그 키워드를 value={keyword}로 사용하면
-// 우리가 input을 조종 할 수 있다.
+
 export default App;
