427f6ac890a6ad55051cf445cff1ded83ebdb208
imonkfcwifi <nohyungkorea@gmail.com>
2022-12-05 11:02:24 +0900

    then vs async 1

--- src/App.js
+++ src/App.js
@@ -1,40 +1,23 @@
+import { useEffect } from "react";
 import { useState } from "react";
 
 function App() {
-  const [toDo, setTodo] = useState("");
-  const [toDos, setTodos] = useState([]);
-  const onChange = (event) => setTodo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year
 
-    setTodos((currenArray) => [toDo, ...currenArray]);
-    setTodo("");
-    // todos.pust <- NEVER
-  };
-  console.log(toDos);
+    `)
+      .then((response) => response.json())
+      .then((json) => {
+        setMovies(json.data.movies);
+        setLoading(false);
+      });
+  }, []);
+
   return (
     <div>
-      <h1>오늘 할 일 {toDos.length} 개</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          text="Text"
-          placeholder="Write your to do"
-        />
-        <button>Add To Do</button>
-      </form>
-      {/* form은 submit 이벤트가 있음 */}
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}> {item} </li>
-        ))}
-        {/* <li key={0}>{item}</li> <li key={1}>{item}</li> <li key={2}>{item}</li> */}
-      </ul>
+      <h1>{loading ? <h1>Loading</h1> : null}</h1>
     </div>
   );
 }
