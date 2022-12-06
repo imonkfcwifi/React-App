39605aa175c34a8bf45ff155eaa64c69cb0e5a9e
imonkfcwifi <nohyungkorea@gmail.com>
2022-12-06 10:48:44 +0900

    details

--- src/App.js
+++ src/App.js
@@ -7,17 +7,17 @@ import {
 import Detail from "./routes/Detail";
 import Home from "./routes/Home";
 
-const router = createBrowserRouter(
+const router1 = createBrowserRouter(
   createRoutesFromElements(
     <Route>
       <Route path="/" element={<Home />} />
-      <Route path="/movie" element={<Detail />} />
+      <Route path="/movie/:id" element={<Detail />} />
     </Route>
   )
 );
 
 function App() {
-  return <RouterProvider router={router} />;
+  return <RouterProvider router={router1} />;
 }
 
 export default App;
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,5 +1,16 @@
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
 const Detail = () => {
+  const { id } = useParams();
+  const getMovie = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+  };
+  useEffect(() => {
+    getMovie();
+  }, []);
   return <h1>Detail</h1>;
 };
-
+// useParams = url에 있는 값을 변환해주는 함수 (특히 변경되는 값)
 export default Detail;
