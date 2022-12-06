cf525465247110238decd5a415b878f217b53517
imonkfcwifi <nohyungkorea@gmail.com>
2022-12-06 14:17:40 +0900

    detail update

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -26,13 +26,14 @@ function Detail() {
         <h1>Loading Details...</h1>
       ) : (
         <div>
-          <h1>Title : {movie.title} </h1>
+          <h1>Title : {movie.title_long} </h1>
           <img src={movie.medium_cover_image} alt={movie.title} />
           <h4>
             {movie.genres
               ? movie.genres.map((g) => <li key={g}>{g}</li>)
               : null}
           </h4>
+          <h4>{movie.runtime} minute </h4>
           <a href={movie.url}>URL : {movie.url}</a>
         </div>
       )}
